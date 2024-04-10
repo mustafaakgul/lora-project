84350cc1966986e451cca86b7c1cac6363275781
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-04-10 14:17:30 +0300

    Upd base

--- src/RN2483-master/Software/RN2483_LoRAWAN_v1.30.X/build/default/production/mcc_generated_files/weather_example.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/mcc_generated_files/weather_example.d  \
- build/default/production/mcc_generated_files/weather_example.p1:  \
- mcc_generated_files/weather_example.c  \
-mcc_generated_files/weather.h 
